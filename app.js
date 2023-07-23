0efe819ff13101396382bd47b42426ccb02bd1a0
Lucas Dipré <lucas.dipre@gmail.com>
2023-07-23 00:08:48 -0300

    Mais uma linah de console testanmdo commit VsCode

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de Jogos")
 console.log("Aplicação esta voando a píncaros de milanesa!!")
+console.log("Aplicação esta voando a píncaros de milanesa2!!")
