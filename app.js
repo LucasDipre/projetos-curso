5ae6536bb32a4b0cde3e658b71b8e5c889282040
Lucas Dipré <lucas.dipre@gmail.com>
2023-07-23 23:10:10 -0300

    Voltando para o estado com duas linhas de console

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de Jogos")
 console.log("Aplicação esta voando a píncaros de milanesa!!")
-console.log("Aplicação esta voando a píncaros de milanesa2!!")
